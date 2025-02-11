53375cee7b3b7854fb2c08969f918dcc3f291ded
Neetidarji <neetidarji5@gmail.com>
2025-02-11 18:02:36 +0530

    assignment 6

---
+++ task6/assignment6/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css'
+import Greeting from "./components/Greeting";
+import ProductInfo from "./components/ProductInfo";
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+      <ProductInfo />
+    </div>
+  );
+}
+
+export default App;
+
