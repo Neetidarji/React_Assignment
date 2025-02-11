baed9f9c3119c235b4e189b15264e020d0c78c7e
Neetidarji <neetidarji5@gmail.com>
2025-02-11 17:40:25 +0530

    assignment5 completed

---
+++ task5/assignment5/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import React from "react";
+import JSXRules from "./components/JSXRules";
+
+function App() {
+  return (
+    <div>
+      <JSXRules />
+    </div>
+  );
+}
+
+export default App;
+
+
